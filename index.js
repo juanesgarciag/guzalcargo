09c4f4f74270b8ec719d25013f8998662ce7ca59
Juanes <juanesgarciag@gmail.com>
2020-07-24 00:53:39 -0500

    Se soluciona bug de overflow

---
+++ index.js
@@ -0,0 +1,38 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+    breakpoints: {
+        640: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 1,
+        },
+        1024: {
+        slidesPerView: 1,
+        },
+    }
+});
