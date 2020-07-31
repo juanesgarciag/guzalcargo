e7f7da88b1b6daa763f59688dbbd51d2a80b6822
Juanes <juanesgarciag@gmail.com>
2020-07-31 13:46:20 -0500

    Se divide css por secciones y hojas individuales, se crea el js del portafolio y se adecuan para todas las pantallas

---
+++ details.js
@@ -0,0 +1,64 @@
+function det1() {
+    document.querySelector('#det1').style.visibility='visible';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det2() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='visible';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det3() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='visible';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det4() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='visible';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det5() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='visible';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det6() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='visible';
+    document.querySelector('#det7').style.visibility='hidden';
+};
+function det7() {
+    document.querySelector('#det1').style.visibility='hidden';
+    document.querySelector('#det2').style.visibility='hidden';
+    document.querySelector('#det3').style.visibility='hidden';
+    document.querySelector('#det4').style.visibility='hidden';
+    document.querySelector('#det5').style.visibility='hidden';
+    document.querySelector('#det6').style.visibility='hidden';
+    document.querySelector('#det7').style.visibility='visible';
+};
+
